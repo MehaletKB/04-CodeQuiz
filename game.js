2004f48dc4663eca736ce28bd22ca4855c6ac565
MehaletKB <mkesatebirhan@gmail.com>
2021-05-30 13:33:58 -0500

    html and css files setup

---
+++ game.js
@@ -0,0 +1,69 @@
+var question = document.querySelector('#question');
+var choices = Array.from(document.getElementsByClassName('choice-text'));
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+
+var allQuestionsAnswers = [
+    {
+    question: "Commonly used data types DO NOT include:",
+    choices: [
+        "strings", 
+        "booleans", 
+        "alerts", 
+        "numbers"],
+    answer: "alerts"
+    },
+
+    {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", 
+    "curly brackets", 
+    "parentheses", 
+    "square brackets"],
+    answer: "parentheses"
+    },
+
+    {
+    question: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"],
+    answer: "all of the above"
+    },
+
+    {
+    question:"String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+    },
+
+    {
+    question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+    }
+]; 
+
+var correctAnswer = 10;
+
+startGame = function(){
+    questionCounter = 0;
+    score = 0;
+    playQuestions = [...allQuestionsAnswers];
+    getQuestion();
+}
+
+getQuestion = function(){
+    questionCounter++;
+    var questionIndex = [] * playQuestions.length;
+    currentQuestion = playQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+
+}
+startGame();
